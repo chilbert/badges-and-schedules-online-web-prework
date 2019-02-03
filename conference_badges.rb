e7479669dbf2440c439a632e5cb34123f976e2c1
Chase Hilbert <chase@heyspacetime.com>
2019-02-03 01:54:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,10 +7,19 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-
   badges = []
   attendees.each do |name|
     badges.push("Hello, my name is #{name}.")
   end
   badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  count = 1
+  attendees.each do |name|
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{counter}.")
+    counter += 1
+  end
+  room_assignments
 end
