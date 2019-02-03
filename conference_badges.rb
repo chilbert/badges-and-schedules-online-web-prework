a860605eb265ec45eaa776e08df8d4ec800722ac
Chase Hilbert <chase@heyspacetime.com>
2019-02-03 01:43:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,12 +6,12 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-# def batch_badge_creator(array_arg)
-#   badge_array = array_arg.to_a
-#
-#   badge_array.each do |name|
-#   puts "Hello, my name is #{name}."
-# end
+def batch_badge_creator(array_arg)
+  badge_array = array_arg.to_a
+
+  badge_array.each do |name|
+  puts "Hello, my name is #{name}."
+end
 
 
 def assign_rooms
