2e28803cd3bd9dad168fc719f7ee281f1d50fe5b
Chase Hilbert <chase@heyspacetime.com>
2019-02-03 01:33:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+#
+#
+
+def batch_badge_creator(array_arg)
+  badge_array = array_arg.to_a
+  badge_array.each |name|
+  puts "Hello, my name is name."
+end
+
+def assign_rooms
+
+end
+
+def printer
+
+end
